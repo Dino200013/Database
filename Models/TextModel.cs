1b807438949b4ba0a8e2a12a780554997e36ccf6
Dino200013 <81235983+Dino200013@users.noreply.github.com>
2021-11-03 16:52:44 +0800

    資料庫管理期末專題

---
+++ Models/TextModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace prjDBHomework.Models
+{
+    public class TextModel
+    {
+        public List<Gallery> gallry { get; set; }
+        public List<Art> art { get; set; }
+        public List<Collects> collects { get; set; }
+        public List<Member> member { get; set; }
+
+        public List<Visits> visits { get; set; }
+
+
+
+    }
+}
